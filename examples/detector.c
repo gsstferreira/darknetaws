4818809aa0ca4faf851b99f6823cbae01dd5ede5
Gustavo S. S. T. Ferreira <37308849+gsstferreira@users.noreply.github.com>
2019-03-13 16:42:09 -0300

    sprite/print changes
    Sprite training images and subsequent changes to cfg files
    
    Change to some of the prints during training

--- examples/detector.c
+++ examples/detector.c
@@ -58,7 +58,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
     pthread_t load_thread = load_data(args);
     double time;
     int count = 0;
-    //while(i*imgs < N*120){
+	double start_time = what_time_is_it_now();
     while(get_current_batch(net) < net->max_batches){
         if(l.random && count++%10 == 0){
             printf("Resizing\n");
@@ -126,22 +126,16 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
         avg_loss = avg_loss*.9 + loss*.1;
 
         i = get_current_batch(net);
-        printf("%ld: %f, %f avg, %f rate, %lf seconds, %d images\n", get_current_batch(net), loss, avg_loss, get_current_rate(net), what_time_is_it_now()-time, i*imgs);
-        if(i%100==0){
-#ifdef GPU
-            if(ngpus != 1) sync_nets(nets, ngpus, 0);
-#endif
-            char buff[256];
-            sprintf(buff, "%s/%s.backup", backup_directory, base);
-            save_weights(net, buff);
-        }
-        if(i%10000==0 || (i < 1000 && i%100 == 0)){
+        printf("\n----- %ld -----  %f loss, %f avg, %f rate, %lf seconds, %d images\n", get_current_batch(net), loss, avg_loss, get_current_rate(net), what_time_is_it_now()-time, i*imgs);
+
+        if(i%1000==0){
 #ifdef GPU
             if(ngpus != 1) sync_nets(nets, ngpus, 0);
 #endif
             char buff[256];
             sprintf(buff, "%s/%s_%d.weights", backup_directory, base, i);
             save_weights(net, buff);
+			printf("Total training time: %f min\n",(what_time_is_it_now() - start_time)/60.0);
         }
         free_data(train);
     }
